a18bd2694425860f1cd7e824df03245985e88f97
Bored Code <97595783+boredecode@users.noreply.github.com>
2022-02-09 07:51:13 +0700

    Create G.py

---
+++ G.py
@@ -0,0 +1,8 @@
+print("   G G G G   ")
+print("  G        G ")
+print(" G           ")
+print(" G           ")
+print(" G     G G G ")
+print(" G         G ")
+print("  G        G ")
+print("   G G G G   ")
